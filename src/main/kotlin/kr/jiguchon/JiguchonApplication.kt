7261c5ee9e721672e5b4227f8f52da0f6615107b
Wordbe <seonghojin3@gmail.com>
2023-09-16 14:33:45 +0900

    init edu server

---
+++ src/main/kotlin/kr/jiguchon/JiguchonApplication.kt
@@ -0,0 +1,11 @@
+package kr.jiguchon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JiguchonApplication
+
+fun main(args: Array<String>) {
+    runApplication<JiguchonApplication>(*args)
+}
---
+++ src/test/kotlin/kr/jiguchon/JiguchonApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.jiguchon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JiguchonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
